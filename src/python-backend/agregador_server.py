9a7a507c477720a6ff8f6d16e15a7f2bb55b721f
Gustavo <gu.ramosp@gmail.com>
2020-10-09 13:40:23 -0300

    Esqueleto da função que realiza as agregações

--- src/python-backend/agregador_server.py
+++ src/python-backend/agregador_server.py
@@ -25,10 +25,24 @@ def _makeAgregation(aggr_request_data):
     # complete_df = fetchData(DATA_PATH,PERIODO,TIPO_INVERSOR)
     print("dentro _makeAgregation")
     complete_df = ftp_params if fetchDataFTP(aggr_request_data) else fetchDataLocal(aggr_request_data)
-    print(complete_df)
-    print(aggr_request_data)
 
-    return "agregações"
+    print(complete_df.shape)
+    complete_df = complete_df.loc[complete_df["mod"] == 1]
+    print(complete_df.shape)
+    
+    for key, value in aggr_request_data.items():
+        if value!="" and (key=="whs" or key== "ene"):
+            #TODO: achar um jeito melhor de escrever isso, e terminar essa lógica de agregação
+            print(complete_df.loc[complete_df["timestamp_iso"] == complete_df.timestamp_iso.max()]['whs'].values[0])
+            print(complete_df.loc[complete_df["timestamp_iso"] == complete_df.timestamp_iso.min()]['whs'].values[0]) 
+            pass
+        elif(value!=""):
+            #TODO: Fazer agregação
+            aggr_request_data[key] = "4"
+            pass
+        print(f"key:{key},value: {value}")
+
+    return aggr_request_data
 
 
 def connectFTP(domain,user,password):
@@ -48,9 +62,9 @@ def fetchDataLocal(aggr_request_data):
     print(local_files_path)
     dataframes_list = []
     # TODO: Converter string de periodo para lista de anos e meses separados
-    data_inicio = aggr_request_data.data_inicio[2:]
-    data_fim    = aggr_request_data.data_fim[2:]
-    tech_type   = aggr_request_data.tech_type
+    data_inicio = aggr_request_data['data_inicio'][2:]
+    data_fim    = aggr_request_data['data_fim'][2:]
+    tech_type   = aggr_request_data['tech_type']
     print(data_inicio,data_fim)
     print(tech_type)
 
@@ -71,13 +85,14 @@ def fetchDataLocal(aggr_request_data):
 
             #Lê arquivos com pandas e adiciona todos em um df
             for file in files:
+                #TODO pegar parte numerica e convert to date time, comparar se esta intevalo  
                 if(pattern.match(file[:-4])):
                     print(file)
                     dataframes_list.append(normalizeDataframes(os.path.join(path,file)))
         complete_df = pd.concat(dataframes_list,ignore_index=True)
         print(complete_df.shape)
         print(complete_df.head())
-        return "complete_df"
+        return complete_df
     except Exception as e:
         print(f"erro:{e}")
         return ("Ocorreu um ero ao buscar arquivos para agregação.\nVerifique se o diretório realmente contém os arquivos.\nErro: {e}")
@@ -95,13 +110,6 @@ def normalizeDataframes(file):
     return df
 
 
-def outer():
-    global ftp_params
-    print("chamou outer")
-    print(ftp_params)
-    ftp_params if print("chamou ftp") else print("chamou local")
-
-
 class AggregationServicer(agregador_pb2_grpc.AggregationServicer):
     def SendParamsFTP(self, request, context):
         global ftp_params
@@ -111,10 +119,9 @@ class AggregationServicer(agregador_pb2_grpc.AggregationServicer):
         local_files_path = None
         response = agregador_pb2.simpleStringResponse()
         response.resposta = "Chamou SendParamsFTP "
-     
-        outer()
         print(ftp_params)
         return response
+
 
     def SendDataPath(self, request, context):
         global ftp_params
@@ -128,16 +135,39 @@ class AggregationServicer(agregador_pb2_grpc.AggregationServicer):
         response.resposta = "Chamou SendDataPath "
         return response
     
+
     def MakeAggregation(self, request, context):
         print("Fazendo agregações")
+        
+        # É, tá assim porque não sei como interar protobuffers e a documentação não ajuda
+        aggr_req_dict = {}
+        aggr_req_dict['data_inicio'] = request.data_inicio
+        aggr_req_dict['data_fim'] = request.data_fim
+        aggr_req_dict['tech_type'] = request.tech_type
+        aggr_req_dict['vdc'] = request.vdc
+        aggr_req_dict['idc'] = request.idc
+        aggr_req_dict['vac'] = request.vac
+        aggr_req_dict['iac'] = request.iac
+        aggr_req_dict['freq'] = request.freq
+        aggr_req_dict['pac'] = request.pac
+        aggr_req_dict['ene'] = request.ene
+        aggr_req_dict['whs'] = request.whs
 
-        print(request)
-        aggregations = _makeAgregation(request)
+        aggregations = _makeAgregation(aggr_req_dict)
 
+        # É, tá assim porque não sei como interar protobuffers e a documentação não ajuda ()
         response = agregador_pb2.Agregregation()
-        response.data_inicio= 'teste' # colocar um por um ou achar um jeito melhor
-
-   
+        response.data_inicio= aggregations['data_inicio']
+        response.data_fim= aggregations['data_fim']
+        response.tech_type= aggregations['tech_type']
+        response.vdc= aggregations['vdc']
+        response.idc= aggregations['idc']
+        request.vac = aggregations['vac']
+        request.iac = aggregations['iac']
+        request.freq = aggregations['freq']
+        request.pac = aggregations['pac']
+        request.ene = aggregations['ene']
+        request.whs = aggregations['whs']
 
         return response
 
